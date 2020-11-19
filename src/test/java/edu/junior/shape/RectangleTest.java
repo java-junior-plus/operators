494959bf2d526cc5b2366abac910ab6d75d51efa
eukovko <eu.kovko@gmail.com>
2020-11-19 12:45:08 +0200

    fixed rectangle test

--- src/test/java/edu/junior/shape/RectangleTest.java
+++ src/test/java/edu/junior/shape/RectangleTest.java
@@ -2,7 +2,7 @@ package edu.junior.shape;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.withinPercentage;
@@ -15,7 +15,7 @@ class RectangleTest {
 
     @DisplayName("Rectangle perimeter test")
     @ParameterizedTest(name = "Test #" + ParameterizedTest.INDEX_PLACEHOLDER)
-    @ValueSource(floats = {13.2F, 0.5F})
+    @CsvSource({"13.2F, 0.5F"})
     void testPerimeter(float sideA, float sideB){
 
         Rectangle rectangle = new Rectangle(sideA, sideB);
@@ -27,7 +27,7 @@ class RectangleTest {
 
     @DisplayName("Rectangle area test")
     @ParameterizedTest(name = "Test #" + ParameterizedTest.INDEX_PLACEHOLDER)
-    @ValueSource(floats = {13.2F, 0.5F})
+    @CsvSource({"13.2F, 0.5F"})
     void testArea(float sideA, float sideB){
 
         Rectangle rectangle = new Rectangle(sideA, sideB);
